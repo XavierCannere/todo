29d9c6074812405c715a2b06af42c8722bf20077
XavierCannere <xavier.cannere2@gmail.com>
2024-02-10 14:23:18 +0100

    date navigator (month)

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,14 +1,29 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function Calendar() {
+    const months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
+    const today = new Date();
+    const [currentMonth, setMonth] = useState(today.getMonth());
 
-  const today = new Date();
+    const beforeClic = () => {
+        setMonth(prevMonth => (prevMonth === 0 ? 11 : prevMonth - 1));
+    };
 
-  return (
-    <div className="Calendar">
-      <h1>{today.toDateString()}</h1>
-    </div>
-  );
+    const afterClic = () => {
+        setMonth(prevMonth => (prevMonth === 11 ? 0 : prevMonth + 1));
+    };
+
+    return (
+        <div className="Calendar">
+            <div className="dateNavigator">
+                <button onClick={beforeClic}>Bouton arrière</button>
+                <h1>{months[currentMonth]}</h1>
+                <button onClick={afterClic}>Bouton avant</button>
+            </div>
+            <p>test</p>
+        </div>
+    );
 }
 
 export default Calendar;
